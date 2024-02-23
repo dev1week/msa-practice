cd6634ef4c9da4940679cf767d2865528600e9ee
the_1_week <the_1_week@naver.com>
2024-02-23 17:39:07 +0900

    feat: api-gateway 연결

--- user-service/src/main/java/com/exmaple/userservice/controller/UserController.java
+++ user-service/src/main/java/com/exmaple/userservice/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 

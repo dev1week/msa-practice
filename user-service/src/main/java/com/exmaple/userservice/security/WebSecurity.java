d5f53283f329c7fae1f2027455285dafa33afe14
dev1week <rondo2860@gmail.com>
2024-02-27 20:21:39 +0900

    feat: 로그인 로직 개발(#5)

--- user-service/src/main/java/com/exmaple/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/exmaple/userservice/security/WebSecurity.java
@@ -1,28 +1,83 @@
 package com.exmaple.userservice.security;
 
+import com.exmaple.userservice.service.UserService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.io.IOException;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity{
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+
+    private final ObjectPostProcessor<Object> objectPostProcessor;
+
+    private static final String[] WHITE_LIST = {
+            "/users/**",
+            "/",
+            "/**"
+    };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests((authz)-> authz.anyRequest().permitAll())
+        http.authorizeHttpRequests(authorize -> {
+                    try {
+                        authorize
+                                .requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .requestMatchers(new IpAddressMatcher("127.0.0.1")).permitAll();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+        )
+                .addFilter(getAuthenticationFilter())
                 .csrf(AbstractHttpConfigurer::disable)
                 //h2 관련 환경설정
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-//        http.headers().frameOptions().disable();
+
         return http.build();
     }
+
+
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception{
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+
+        return authenticationFilter;
+
+    }
+
 
 }
--- user-service/src/main/java/com/exmaple/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/exmaple/userservice/service/UserServiceImpl.java
@@ -10,10 +10,12 @@ import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -75,5 +77,12 @@ public class UserServiceImpl implements UserService{
     @Override
     public Iterable<UserEntity> getUserByAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException(username));
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPwd(),
+                true, true, true, true, new ArrayList<>());
     }
 }

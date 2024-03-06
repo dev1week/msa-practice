8f9bcf15387cb730c7bc5c451df9683c9344fe6a
the_1_week <the_1_week@naver.com>
2024-03-06 13:12:18 +0900

    feat: 로그인 로직 구현

--- user-service/src/main/java/com/exmaple/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/exmaple/userservice/security/WebSecurity.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -20,6 +21,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.IOException;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -27,19 +30,22 @@ import java.io.IOException;
 public class WebSecurity{
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
 
 
     private final ObjectPostProcessor<Object> objectPostProcessor;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests((authz)-> authz.anyRequest())
-                .addFilter(getAuthenticationFilter())
-                .csrf(AbstractHttpConfigurer::disable)
-                //h2 관련 환경설정
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
-//        http.headers().frameOptions().disable();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf->csrf.disable());
+        http.authorizeHttpRequests(request->{
+            //gateway에서 호출시 해당 URL 수정해야합니다. /users로 수정합니다.
+            request.requestMatchers(antMatcher("/user-service/users/**")).permitAll();
+            request.requestMatchers(antMatcher("/h2-console/**")).permitAll();
+        });
+        http.headers(headers->headers.frameOptions(frameOptions->frameOptions.disable()));
+        http.addFilter(getAuthenticationFilter());
+
         return http.build();
     }
 
@@ -53,7 +59,7 @@ public class WebSecurity{
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception{
 
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(userService, env);
 
         AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
         authenticationFilter.setAuthenticationManager(authenticationManager(builder));
